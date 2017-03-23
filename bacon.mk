5063fa35c11af46cb472561cf958d7ce5192b220
KalilDev <kalilgamer0@gmail.com>
2017-03-23 03:37:08 +0000

    Pixel launcher. why? cuz fuck launcher3/2

--- bacon.mk
+++ bacon.mk
@@ -117,6 +117,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.msm8974
 
+# Launcher
+PRODUCT_PACKAGES += \
+    PixelLauncher \
+    WallpaperPickerGoogle
+
 # Media profile
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
